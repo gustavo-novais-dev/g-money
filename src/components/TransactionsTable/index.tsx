3ec33583fe9bc62779522e6465e95503d39c54eb
Gustavo Novais <gusttavo.developer@gmail.com>
2022-03-27 21:46:05 -0300

    Creating the Transaction Table component

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,35 @@
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+    return (
+        <Container>
+            <table>
+                <thead>
+                <tr>
+                    <th>Título</th>
+                    <th>Valor</th>
+                    <th>Categoria</th>
+                    <th>Data</th>
+                </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>Desenvolvimento de webSite</td>
+                        <td className="deposit">R$ 12.000</td>
+                        <td>Desenvolvimento</td>
+                        <td>27/03/2022</td>
+                    </tr>
+
+                    <tr>
+                        <td>Aluguel</td>
+                        <td  className="withdraw">- R$ 2.000</td>
+                        <td>Casa</td>
+                        <td>27/03/2022</td>
+                    </tr>
+
+                </tbody>
+            </table>
+        </Container>
+    )
+}
