a8c2bf57a0f97f8cbad7c55fe104ee5ba5c78070
Gustavo Novais <gusttavo.developer@gmail.com>
2022-03-29 19:53:46 -0300

    Accessibility

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
+Modal.setAppElement("#root");
+
 export function App() {
 
   const [isNewTransactionsModalOpen, setIsNewTransactionsModalOpen] = useState(false);
