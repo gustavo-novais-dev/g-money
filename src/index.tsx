8d0270753a720bc982723d72b4641011bd0cba81
Gustavo Novais <gusttavo.developer@gmail.com>
2022-04-02 15:25:05 -0300

    Inserting transaction into the API

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs';
-import {App} from './App';
+import { createServer, Model } from 'miragejs';
+import { App } from './App';
 
 createServer({
- routes(){
-   this.namespace = 'api';
-   this.get('/transactions', () =>{
-     return [
-       {
-         id: 1,
-         title: 'Transaction 1',
-         Amount: 400,
-         type: 'deposit',
-         category: 'Food',
-         CreateDt: new Date()
-       }
-     ]
-   })
- }
+  models: {
+    transaction: Model,
+  },
+
+  routes() {
+    this.namespace = 'api';
+    
+    this.get('/transactions', () => {
+      return this.schema.all('transaction')
+    })
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data);
+    })
+
+  }
 })
 
 ReactDOM.render(
