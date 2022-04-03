d2866c8a51fb659e65ed502ddfcef5a5a1881fd1
Gustavo Novais <gusttavo.developer@gmail.com>
2022-04-03 19:02:12 -0300

    Setting date and currency values

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -18,30 +18,39 @@ export function TransactionsTable() {
 
     useEffect(() => {
         api.get('transactions')
-        .then(response => setTransactions(response.data.transactions))
+            .then(response => setTransactions(response.data.transactions))
     }, [])
 
     return (
         <Container>
             <table>
                 <thead>
-                <tr>
-                    <th>Título</th>
-                    <th>Valor</th>
-                    <th>Categoria</th>
-                    <th>Data</th>
-                </tr>
+                    <tr>
+                        <th>Título</th>
+                        <th>Valor</th>
+                        <th>Categoria</th>
+                        <th>Data</th>
+                    </tr>
                 </thead>
 
                 <tbody>
-                 
+
                     {transactions.map(transaction => {
                         return (
                             <tr key={transaction.id}>
                                 <td>{transaction.title}</td>
-                                <td className={transaction.type}>{transaction.amount}</td>
+                                <td className={transaction.type}>
+                                    {new Intl.NumberFormat('pt-BR', {
+                                        style: 'currency',
+                                        currency: 'BRL'
+                                    }).format(transaction.amount)}
+                                </td>
                                 <td>{transaction.category}</td>
-                                <td>{transaction.createdAt}</td>
+                                <td>
+                                    {new Intl.DateTimeFormat('pt-BR').format(
+                                        new Date(transaction.createdAt)
+                                    )}
+                                </td>
                             </tr>
                         )
                     })}
